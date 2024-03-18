0fabdd46b3d87c58292cadd0dfc79a8f54c19762
Ce34Rod <139182790+Ce34Rod@users.noreply.github.com>
2024-03-17 21:36:52 -0500

    Initial

---
+++ src/main/java/com/pineapplemenu/PineappleMenu/PineappleMenuApplication.java
@@ -0,0 +1,14 @@
+package com.pineapplemenu.PineappleMenu;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PineappleMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PineappleMenuApplication.class, args);
+    }
+
+}

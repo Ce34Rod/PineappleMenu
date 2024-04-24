a36bcb3acef34d49bfc22c97252e3ddf9ae876d1
Ce34Rod <139182790+Ce34Rod@users.noreply.github.com>
2024-04-23 21:26:32 -0500

    parallax scrolling #2

--- src/main/java/com/pineapplemenu/PineappleMenu/controllers/modelExchangeController.java
+++ src/main/java/com/pineapplemenu/PineappleMenu/controllers/modelExchangeController.java
@@ -1,24 +1,16 @@
 package com.pineapplemenu.PineappleMenu.controllers;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class modelExchangeController {
 
-    @GetMapping("/")
+    @GetMapping("modelExchange")
     public String slideshow(Model model){
         return "modelExchange";
     }
 
-    @GetMapping("/jessica-lauren")
-    public String jessicalauren(Model model){
-        return "jessica-lauren";
-    }
-
-    @GetMapping("/slidingWindow")
-    public String slidingWindow (Model model) {
-        return "slidingWindow";
-    }
 }
